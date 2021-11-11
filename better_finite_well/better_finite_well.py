b13aee9220bbb642d039b940a1d5cdf90d801e11
Troy Shepherd <92545191+tshepherd3435@users.noreply.github.com>
2021-11-11 13:57:41 +0000

    Added improved finite well simulation, code to time different implementations and various plots

---
+++ better_finite_well/better_finite_well.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 20:39:30 2021
+
+@author: Troy
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 27 20:24:20 2021
+
+@author: Troy
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Particle in a box
+# Atomic units so numbers stay in sensible range
+
+hbar = 1
+m = 1
+L = 1                    # Atomic units for each
+D = (1j * hbar)/(2 * m)  # Coefficient on second spatial derivative, named to mirror diffusion code
+V0 = float(input('Potential? '))
+dx = 0.01
+dt = 0.01*(dx**2) / abs(D)
+x = np.linspace(-L, L, 101)                                                    # Discretising domain
+dx = x[1] - x[0]                                                               # Defining grid spacing
+psi_left = 0 + 0j                                                              # Chosen to approximate psi asymptote
+psi_right = 0 + 0j
+V = np.zeros(len(x))
+
+for i in range(0, len(x)):
+    if x[i] < -L/4 or L/4 < x[i]:
+        V[i] = V0
+
+def initial_condition(x):                       # Simple Gaussian function, corresponding to particle's position being roughly known
+    return (np.sqrt(2/L)*np.sin(np.pi * x))
+
+def nsd(psi, dx):
+    d2f = np.zeros(len(x))
+    d2f = d2f.astype('complex64')
+    for i in range(1, len(x)-1):
+        d2f[i] = (psi[i+1] + psi[i-1] -2*psi[i])
+    d2f = d2f/(dx**2)
+    return d2f
+
+def update(psi, dx, dt, D):
+    d_psi = (D * nsd(psi, dx) - 1j/hbar * V * psi)* dt
+    return d_psi
+
+def time_propagate(T, dx, dt, D, psi):
+    t = 0
+    while t<T:
+        d_psi = update(psi, dx, dt, D)
+        psi += d_psi
+        t += dt
+        psi[0] = psi_left
+        psi[-1] = psi_right
+    return psi
+
+psi = initial_condition(x) + 0j*initial_condition(x)
+
+norms = []
+
+for ii in range(0, 11):
+    psi = time_propagate(ii*100*dt, dx, dt, D, psi) # propagate for 100 time steps, then stop and plot
+    # norms.append(np.linalg.norm(psi))
+    # plt.plot(x,np.abs(psi)**2, label ="T="+str(ii*100)+"dt") # plot probability
+    plt.figure(ii)
+    plt.plot(x, np.real(psi), label = r'Re($\Psi$)')
+    plt.plot(x, np.imag(psi), label = r'Im($\Psi$)')
+    plt.legend()
+    plt.title(f'T = {ii*100*dt}')
+
+# plt.legend()
+# plt.title(f'V = {V0} Ha')

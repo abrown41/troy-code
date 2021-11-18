ed5dddfd20750f8fbd86db8b03c70cd23ce0391f
Troy Shepherd <92545191+tshepherd3435@users.noreply.github.com>
2021-11-18 14:07:41 +0000

    Timing programs now propagate to the same final time regardless of dt. Added plots produced by all combinations of paramaters.

--- timed_plots/timing_5_point.py
+++ timed_plots/timing_5_point.py
@@ -7,10 +7,13 @@ Created on Wed Nov 10 22:44:20 2021
 
 import timeit
 
+N = int(input('Iterations: '))
+
 setup_code = '''
 import numpy as np
 import matplotlib.pyplot as plt
 
+iteration = 1
 hbar = 1
 m = 1
 L = 1                    # Atomic units for each
@@ -19,10 +22,10 @@ V0 = 1000
 dx = float(input('Enter spatial grid spacing: '))
 dt = float(input('Enter time step: '))
 x = np.arange(-L, L+dx, dx)                                                    # Discretising domain
-dx = x[1] - x[0]                                                               # Defining grid spacing
 psi_left = 0 + 0j                                                              # Chosen to approximate psi asymptote
 psi_right = 0 + 0j
 V = np.zeros(len(x))
+T = 0.002
 
 for i in range(0, len(x)):
     if x[i] < -L/4 or L/4 < x[i]:
@@ -36,7 +39,7 @@ def nsd(psi, dx):
     d2f = d2f.astype('complex64')
     for i in range(2, len(x)-2):
         d2f[i] = (-1/12 * psi[i-2]) + (4/3 * psi[i-1]) + (-2.5 * psi[i]) + (4/3 * psi[i+1]) + (-1/12 * psi[i+2])
-    d2f = d2f/(12 * dx**2)
+    d2f = d2f/(dx**2)
     return d2f
 
 def update(psi, dx, dt, D):
@@ -57,19 +60,21 @@ program = '''
 psi = initial_condition(x) + 0j*initial_condition(x)
 
 norms = []
-
+plt.figure(iteration)
 for ii in range(0, 11):
-    psi = time_propagate(ii*100*dt, dx, dt, D, psi) # propagate for 100 time steps, then stop and plot
+    psi = time_propagate(ii*(T/10), dx, dt, D, psi) # propagate for 100 time steps, then stop and plot
     # norms.append(np.linalg.norm(psi))
     plt.plot(x,np.abs(psi)**2, label ="T="+str(ii*100)+"dt") # plot probability
     # plt.figure(ii)
     # plt.plot(x, np.real(psi), label = r'Re($\Psi$)')
     # plt.plot(x, np.imag(psi), label = r'Im($\Psi$)')
     # plt.legend()
-    # plt.title(f'T = {ii*100*dt}')
+    # plt.title(f'T = {ii*(T/10)}')
 
 plt.legend()
 plt.title(f'V = {V0} Ha')
+
+iteration += 1
 '''
 
-print(f"Executed in {timeit.timeit(stmt= program, setup = setup_code, number=1)} seconds")
+print(f"Average execution: {timeit.timeit(stmt= program, setup = setup_code, number=N) / N} seconds")
